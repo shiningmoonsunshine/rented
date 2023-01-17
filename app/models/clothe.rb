765bde37bc6fd6ffb9e2fd80eb2be96c3d116aa4
Sabrina Achab <sabrina.achab@hotmail.com>
2023-01-17 21:30:15 +0100

    Created clothes model with controller, need to add routes new and view

--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -1,4 +1,5 @@
 class Clothe < ApplicationRecord
   belongs_to :booking
   belongs_to :user
+  validates :category, :description, :price, :user, :booking, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :bookings, only: [:index, :new, :create]
+  resources :clothes, only: [:index, :show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
