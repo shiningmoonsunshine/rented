bef1d5a07d2b57d836ec86d831b00dcad1432f7c
SabrinaAssya <sabrina.achab@hotmail.com>
2023-01-21 17:20:18 +0100

    dashboard view done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,6 @@ class PagesController < ApplicationController
 
   def dashboard
     @bookings = Booking.all
-    @clothe_id = @booking.clothe_id
-    @clothe = Clothe.find(@clothe_id)
   end
 
 end
