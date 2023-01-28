04bb73f711833d5bccb3079883d86b8a4d73c292
SabrinaAssya <sabrina.achab@hotmail.com>
2023-01-28 11:54:41 +0100

    working on dashboard css and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,16 @@ class PagesController < ApplicationController
 
   def dashboard
     @bookings = Booking.all
+    def booked_or_not
+      @bookings.each do |b|
+        if b.user_id == current_user.id
+          true
+        else
+          false
+        end
+      end
+    end
   end
+
 
 end
