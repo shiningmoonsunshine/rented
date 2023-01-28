895ef987887262f6ca910c309e81dc252d1565c4
oriane corral <shining.moonsunshine@gmail.com>
2023-01-28 13:01:26 +0100

    added pg search

--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -3,4 +3,10 @@ class Clothe < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
   validates :title, :category, :description, :price, :address, presence: true
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+  against: [ :title, :description ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
